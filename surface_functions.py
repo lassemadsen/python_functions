3eb51a895124792b7c79c317c75e946340fc4f9b
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2021-11-11 14:36:34 +0100

    Added unpaired and paired t-test (using brainstat)

--- surface_functions.py
+++ surface_functions.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 PUBLIC_PATH='/public/lama'
 
+# ----- ROI functions ------
 def get_roi_mask(aal_list):
     """Get mask of anatomical areas
     Explanation of anatomical atlas areas can be found in /public/lama/data/surface/aal_full.txt
@@ -43,5 +44,67 @@ def lookup_roi(aal_area):
 
     return name
 
+# ----- Brainstat functions ------
+from brainspace.mesh.mesh_io import read_surface
+from brainstat.stats.terms import FixedEffect
+from brainstat.stats.SLM import SLM
 
+surf = {'left': read_surface('/Users/au483096/Documents/PhD/data/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_left_smooth.gii'),
+        'right': read_surface('/Users/au483096/Documents/PhD/data/surface_data/mni_icbm152_t1_tal_nlin_sym_09c_right_smooth.gii')}
+
+def unpaired_ttest(data_group1, data_group2):
+
+    result = {'left': [], 'right': []}
+
+    group1_subjects = data_group1['left'].columns
+    group2_subjects = data_group2['left'].columns
+
+    group1 = pd.DataFrame(np.concatenate([np.ones(len(group1_subjects)), np.zeros(len(group2_subjects))]), columns=['group1'])
+    group2 = pd.DataFrame(np.concatenate([np.zeros(len(group1_subjects)), np.ones(len(group2_subjects))]), columns=['group2'])
+
+    for hemisphere in ['left', 'right']:
+        data = pd.concat([data_group1[hemisphere], data_group2[hemisphere]], axis=1).T
+
+        term_group1 = FixedEffect(group1)
+        term_group2 = FixedEffect(group2)
+
+        model = term_group1 + term_group2
+        contrast = term_group2.group2 - term_group1.group1
+
+        slm = SLM(model, contrast, surf=surf[hemisphere])
+        slm.fit(data.values)
+
+        result[hemisphere] = slm
+    
+    print(f'Group 1: N={len(group1_subjects)}, group 2: N={len(group2_subjects)}')
+
+    return result
+
+def paired_ttest(data1, data2):
+    result = {'left': [], 'right': []}
+
+    common_subjects = sorted(list(set(data1['left'].columns) & set(data2['left'].columns)))
+
+    measurement_1 = pd.DataFrame(np.concatenate([np.ones(len(common_subjects)), np.zeros(len(common_subjects))]), columns=['measurment_1'])
+    measurement_2 = pd.DataFrame(np.concatenate([np.zeros(len(common_subjects)), np.ones(len(common_subjects))]), columns=['measurment_2'])
+    subjects = pd.DataFrame(np.tile(np.eye(len(common_subjects)), 2).T, columns=common_subjects)
+
+    for hemisphere in ['left', 'right']:
+        data = pd.concat([data1[hemisphere][common_subjects], data2[hemisphere][common_subjects]], axis=1).T
+
+        term_meas1 = FixedEffect(measurement_1, add_intercept=False)
+        term_meas2 = FixedEffect(measurement_2, add_intercept=False)
+        term_subject = FixedEffect(subjects, add_intercept=False)
+
+        model = term_meas1 + term_meas2 + term_subject
+        contrast = term_meas2.measurment_2 - term_meas1.measurment_1
+
+        slm = SLM(model, contrast, surf=surf[hemisphere])
+        slm.fit(data.values)
+
+        result[hemisphere] = slm
+    
+    print(f'N={len(common_subjects)}')
+    
+    return result
 
