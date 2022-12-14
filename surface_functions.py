98fcd389d28bd5880de0d87bbabddb8af8cf99f0
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2022-12-14 21:55:58 +0100

    Statistical analysis now return information on cluster surviving fwer correction, such as corrected p-value, mni coordinates, and cluster area

--- surface_functions.py
+++ surface_functions.py
@@ -1,6 +1,7 @@
 """This script contain useful funcitons when working with cortical surface data in python
 """
 
+from unicodedata import name
 import numpy as np
 import pandas as pd
 import os
@@ -16,6 +17,11 @@ if not os.path.isdir(PUBLIC_PATH):
 
 SURFACE_GII = {'left': f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_left_smooth.gii',
                'right': f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_right_smooth.gii'}
+
+ATLAS_LABELS = {'left': f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_left_smooth_ana_clean.labels',
+               'right': f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_right_smooth_ana_clean.labels'}
+
+ATLAS_LOOKUP = f'{PUBLIC_PATH}/data/surface/aal_full.txt'
 
 # ----- ROI functions ------
 def get_roi_mask(aal_list):
@@ -33,7 +39,7 @@ def get_roi_mask(aal_list):
            'right': []}
 
     for hemisphere in ['left', 'right']:
-        labels = np.loadtxt(f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_{hemisphere}_smooth_ana_clean.labels', skiprows=1)
+        labels = np.loadtxt(ATLAS_LABELS[hemisphere], skiprows=1)
 
         roi[hemisphere] = np.isin(labels, [aal_list])+0 # +0 to make 1/0 instead of true/false
 
@@ -49,7 +55,7 @@ def lookup_roi(aal_area):
         Value of anatomical area
     """
 
-    aal_full = pd.read_csv(f'{PUBLIC_PATH}/data/surface/aal_full.txt', names=['val', 'name'])
+    aal_full = pd.read_csv(ATLAS_LOOKUP, names=['val', 'name'])
 
     name = aal_full[aal_full.val == aal_area].name.squeeze().replace(' ', '_')
 
@@ -63,10 +69,12 @@ from brainstat.stats.SLM import SLM
 surf = {'left': read_surface(f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_left_smooth.gii'),
         'right': read_surface(f'{PUBLIC_PATH}/data/surface/mni_icbm152_t1_tal_nlin_sym_09c_right_smooth.gii')}
 
-def unpaired_ttest(data_group1, data_group2, correction=None, cluster_threshold=0.001, alpha=0.05):
+def unpaired_ttest(data_group1, data_group2, covars=None, correction=None, cluster_threshold=0.001, alpha=0.05):
     """
     alpha : float | 0.05
         Threshold of corrected clusters
+    covars : pandas df
+        Dataframe with covariates (one per row) and header is id (matching ids in data_group1 and data_group2)
     """
     result = {'left': [], 'right': []}
 
@@ -85,6 +93,13 @@ def unpaired_ttest(data_group1, data_group2, correction=None, cluster_threshold=
         term_group2 = FixedEffect(group2)
 
         model = term_group1 + term_group2
+
+        if covars is not None:
+            for covar in covars.index:
+                covar_term = FixedEffect(covars.loc[covar][group1_subjects].append(covars.loc[covar][group2_subjects]).values, names=covar)
+
+                model = model + covar_term
+
         contrast = term_group2.group2 - term_group1.group1
 
         slm = SLM(model, contrast, surf=surf[hemisphere], correction=correction, cluster_threshold=cluster_threshold, mask=mask)
@@ -95,8 +110,9 @@ def unpaired_ttest(data_group1, data_group2, correction=None, cluster_threshold=
     print(f'Group 1: N={len(group1_subjects)}, group 2: N={len(group2_subjects)}')
 
     cluster_mask = get_cluster_mask(result, correction, alpha)
+    cluster_summary = get_cluster_summary(result)
 
-    return result, cluster_mask
+    return result, cluster_mask, cluster_summary
 
 def paired_ttest(data1, data2, correction=None, cluster_threshold=0.001, alpha=0.05):
     """
@@ -131,8 +147,9 @@ def paired_ttest(data1, data2, correction=None, cluster_threshold=0.001, alpha=0
     print(f'N={len(common_subjects)}')
 
     cluster_mask = get_cluster_mask(result, correction, alpha)
+    cluster_summary = get_cluster_summary(result)
     
-    return result, common_subjects, cluster_mask
+    return result, common_subjects, cluster_mask, cluster_summary
 
 def correlation(surface_data, predictors, correction=None, cluster_threshold=0.001, alpha=0.05):
     """
@@ -177,8 +194,9 @@ def correlation(surface_data, predictors, correction=None, cluster_threshold=0.0
         result[hemisphere] = slm
     
     cluster_mask = get_cluster_mask(result, correction, alpha)
+    cluster_summary = get_cluster_summary(result)
     
-    return result, common_subjects, cluster_mask
+    return result, common_subjects, cluster_mask, cluster_summary
 
 def correlation_pearson(param, predictor):
     """
@@ -294,8 +312,9 @@ def correlation_other_surface(surface_data, surface_data_predictor, predictor_na
         result[hemisphere] = slm
 
     cluster_mask = get_cluster_mask(result, correction, alpha)
+    cluster_summary = get_cluster_summary(result)
 
-    return result, common_subjects, cluster_mask
+    return result, common_subjects, cluster_mask, cluster_summary
     
 def get_cluster_mask(result, correction, alpha):
     if correction is not None:
@@ -308,3 +327,103 @@ def get_cluster_mask(result, correction, alpha):
 
     return cluster_mask
 
+def get_cluster_summary(result):
+    """Calculate summary of surviving clusters
+    
+    Return information on cluster area (mm^2), cluster_id, cluster location (MNI coordinates) and cluster corrected p-value
+    """
+
+    cluster_summary = pd.DataFrame({'hemisphere': [], 'clusid': [], 'cluster_area_mm2': [], 'mni_coord': [], 'anatomical_location': [], 'clus_pval_fwer': []})
+
+    aal_full = pd.read_csv(ATLAS_LOOKUP, names=['val', 'name'])
+
+    for hemisphere in ['left', 'right']:
+        mni_coord = result[hemisphere].surf.Points
+        labels = np.loadtxt(ATLAS_LABELS[hemisphere], skiprows=1)
+        for posneg in [0, 1]:
+            cluster_survived = result[hemisphere].P['clus'][posneg][result[hemisphere].P['clus'][posneg].P < 0.05]
+
+            if cluster_survived.empty:
+                continue
+
+            for clusid in cluster_survived.clusid:
+                clus_pval = result[hemisphere].P['clus'][posneg][result[hemisphere].P['clus'][posneg].clusid == clusid].P.values[0]
+                peak_vertex = list(result[hemisphere].P['peak'][posneg][result[hemisphere].P['peak'][posneg].clusid == clusid].vertid)[0]
+
+                anatomical_label = labels[peak_vertex]
+                anatomical_loc = aal_full[aal_full.val == anatomical_label].name.squeeze()
+
+                peak_coord = mni_coord[peak_vertex]
+                peak_coord = [round(c) for c in peak_coord] # Round coordinates
+                
+                idx = np.where(result[hemisphere].P['clusid'][posneg][0] == clusid)[0]
+                polys = result[hemisphere].surf.polys2D[np.isin(result[hemisphere].surf.polys2D, idx).all(axis=1)]
+
+                area = 0
+                for p in polys:
+                    a = mni_coord[p[0]]
+                    b = mni_coord[p[1]]
+                    c = mni_coord[p[2]]
+
+                    x = np.cross((a - b), (b - c))
+                    A = np.sqrt(x.dot(x)) / 2
+
+                    area += A
+
+                cluster_summary = cluster_summary.append({'hemisphere': hemisphere, 'clusid': clusid, 'cluster_area_mm2': f'{area:.0f}', 'mni_coord': peak_coord, 'anatomical_location': anatomical_loc, 'clus_pval_fwer': clus_pval}, ignore_index=True)
+
+    return cluster_summary
+
+
+# def find_clusters(stat_data, threshold, threshold_is_min=False):
+#     """Segments cortical surface p-map at given threshold and finds surviving clusters
+#     Will do this for both hemispheres
+
+#     stat_data : dict{'left', 'right'}
+#         Statistical data for left and right hemisphere (e.g. pval, tval or zscore)
+#     threshold : float
+#         Value to threshold the statistical data
+#     threshold_is_min : bool | False
+#         If true, threshold is used as lower limit. Default is upper limit. 
+#     """
+
+#     indexed_clusters = {'left': np.zeros(len(stat_data['left']), ),
+#                                 'right': np.zeros(len(stat_data['right']), )}
+#     size = {'left': {}, 'right': {}}
+
+#     for hemisphere in ['left', 'right']: 
+#         surf = read_surface(SURFACE_GII[hemisphere])
+#         faces_all = surf.polys2D
+#         vert_idx = np.arange(faces_all.max() + 1)
+
+#         if threshold_is_min:
+#             not_used_indexes = set(vert_idx[stat_data[hemisphere].ravel() >= threshold])
+#         else:
+#             not_used_indexes = set(vert_idx[stat_data[hemisphere].ravel() <= threshold])
+
+#         # == Only used faces containing not_used indexes ==
+#         faces = faces_all[np.isin(faces_all, list(not_used_indexes)).any(axis=1)]
+
+#         # -------------------------
+#         # ----- Find clusters -----
+#         # -------------------------
+#         cluster_val = 1
+#         while not_used_indexes:
+#             in_cluster = {not_used_indexes.pop()}
+#             neighbours = set(faces[np.isin(faces, list(in_cluster)).any(axis=1)].ravel()) & not_used_indexes
+#             in_cluster = in_cluster | neighbours
+
+#             while True:
+#                 neighbours = set(faces[np.isin(faces, list(neighbours)).any(axis=1)].ravel()) & not_used_indexes
+#                 not_used_indexes = not_used_indexes - neighbours
+#                 if len(neighbours) == 0:
+#                     break
+#                 else:
+#                     in_cluster = in_cluster | neighbours
+
+#             indexed_clusters[hemisphere][list(in_cluster)] = cluster_val
+#             size[hemisphere][cluster_val] = len(in_cluster)
+
+#             cluster_val = cluster_val + 1
+
+#     return indexed_clusters, size
