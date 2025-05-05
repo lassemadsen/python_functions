7e664ac264aade498ad058336bccbfaaba2f4c63
Lasse Stensvig Madsen <lasse.madsen93@gmail.com>
2025-05-05 08:51:15 +0200

    Added stormdb_functions

---
+++ stormdb_functions.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import pickle
+import os
+import numpy as np
+from tempfile import TemporaryDirectory
+from glob import glob
+import dicom2nifti
+
+def convert_pwi(filtered_serie: dict, outfile: str, clobber: bool = False):
+    if not os.path.isfile(outfile) or clobber:
+
+        with TemporaryDirectory() as tmp_dir:
+            # CONVERT IMG DATA
+            dicom2nifti.convert_directory('/Volumes' + filtered_serie['path'], tmp_dir, compression=False)
+
+            # Rename output file
+            out_file_temp = glob(tmp_dir + '/*.nii')[0]
+
+            Path(os.path.dirname(outfile)).mkdir(parents=True, exist_ok=True)
+            os.rename(out_file_temp, outfile)
+    else:
+        print(f'{outfile} exits. Use clobber to overwrite.')
+
+    # INFO
+    dcm_file = f'/Volumes/{filtered_serie["path"]}/{filtered_serie["files"][0]}'
+    info = get_info(dcm_file)
+
+    return info
+
+
+def get_info(dcm_file):
+    import pydicom
+    ds = pydicom.dcmread(dcm_file)
+
+    info = {}
+
+    # --- Echo time ---
+    echoTime = ds.EchoTime
+
+    # --- Repetition time ---
+    repetitionTime = ds.RepetitionTime
+
+    # --- Aquisition times ---
+    for element in ds:
+        if "MosaicRefAcqTimes" in element.name:
+            acqtime = element.value
+
+    acqorder = np.argsort(acqtime)
+
+    uniq_acqtime = list(dict.fromkeys(acqtime))
+
+    _, uniq_acqorder = np.unique(uniq_acqtime, return_index=True)
+
+    for i, time in enumerate(uniq_acqtime):
+        acqorder[np.array(acqtime) == time] = uniq_acqorder[i]
+    
+    # --- Return ---
+    info['TE'] = echoTime * 1e-3 # In seconds
+    info['TR'] = repetitionTime * 1e-3 # In seconds
+    info['acqorder'] = acqorder
+    
+    return info
